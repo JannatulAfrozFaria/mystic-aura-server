4b8fa8ef434a28a3c3ee41e7d7c4dd3d71567275
Janntul Afroz Faria <afroz_faria03@yahoo.com>
2024-10-10 01:30:07 +0600

    app.listen command added to server

--- index.js
+++ index.js
@@ -8,3 +8,6 @@ app.use(express.json());
 app.get('/', (req,res)=>{
     res.send('mystic aura is running')
 } )
+app.listen(port, ()=>{
+    console.log(`Mystic Aura is running on port ${port}`);
+})
