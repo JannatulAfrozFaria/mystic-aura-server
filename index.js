7fa145987c085d24205d4d747a865c6d8630cef5
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-05-22 17:23:01 +0600

    delete api created for users

--- index.js
+++ index.js
@@ -90,6 +90,12 @@ async function run() {
       const result = await userCollection.insertOne(user);
       res.send(result);
     });
+    app.delete('/users/:id', async(req,res)=>{
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id)}
+      const result= await userCollection.deleteOne(query);
+      res.send(result);
+    })
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log(
