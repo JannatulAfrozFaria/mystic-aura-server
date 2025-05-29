a6055aa43afe866bd62005f2149b8977257b6db6
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-05-30 04:08:06 +0600

    create api for jwt

--- index.js
+++ index.js
@@ -31,6 +31,13 @@ async function run() {
     const cartCollection = client.db("mysticDB").collection("carts");
     const userCollection = client.db("mysticDB").collection("users");
 
+    //jwt related api
+    app.post('/jwt', async(req,res)=>{
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET,{expiresIn: '1h'} );
+      res.send({token});
+    })
+
     app.get("/perfumes", async (req, res) => {
       const result = await perfumeCollection.find().toArray();
       res.send(result);
