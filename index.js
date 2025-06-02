8b08cdb01410fdd1a04c9478628318f88a49d835
Jannatul Afroz Faria <afroz_faria03@yahoo.com>
2025-06-03 01:49:12 +0600

    token verification logic added

--- index.js
+++ index.js
@@ -43,7 +43,14 @@ async function run() {
       if(!req.headers.authorization){
         return res.status(401).send({message: 'forbidden access' })
       }
-      // next();
+      const token = req.headers.authorization.split(' ')[1];
+      jwt.verify(token,process.env.ACCESS_TOKEN_SECRET, (err,decoded)=>{
+        if(err){
+          return res.status(401).send({message: 'forbidden access'})
+        }
+        req.decoded = decoded;
+        next();
+      } )
     }
 
     app.get("/perfumes", async (req, res) => {
